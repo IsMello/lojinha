931521a9ecba520b800c2dafaa6bbdb0897052d2
thais <thais.de.m.damasio@accenture.com>
2020-05-11 19:34:43 -0300

    added product list

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/products', function(req,res,next) {
+
+  var produtos = [{
+    "name":"sabonete",
+    "description": "sabonete protex",
+    "price": "1.99"
+  },
+  {
+    "name":"leite",
+    "description": "leite ninho",
+    "price": "7.8"
+  },
+  {
+    "name":"panela",
+    "description":"panela tramontina",
+    "price":"150.99"
+  },{
+    "name":"chinelos",
+    "description":"par de chinelos havaianas",
+    "price":"20.50"
+  }]
+
+  res.json(produtos);
+});
+
 module.exports = router;
